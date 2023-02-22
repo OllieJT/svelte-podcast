8afc23cbac9290e9f5debb87f1da7f2f87e598a3
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-02-22 00:29:04 +0000

    logs progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,15 @@
 <h1>Demo</h1>
 
 <h5>Native audio controls</h5>
-<Audio src="/example.mp3" autoplay={false} {volume} {playbackRate} {paused} bind:currentTime />
+<Audio
+	on:progress={(e) => console.log(e.detail)}
+	src="/example.mp3"
+	autoplay={false}
+	{volume}
+	{playbackRate}
+	{paused}
+	bind:currentTime
+/>
 
 <h5>Custom audio controls</h5>
 
@@ -25,7 +33,6 @@
 
 <button type="button" on:click={() => (currentTime = currentTime + 5)}>Skip 5s</button>
 <button type="button" on:click={() => (currentTime = currentTime - 5)}>Rewind 5s</button>
-
 <button type="button" on:click={() => (currentTime = 30)}>Go to 30s</button>
 
 <h6>Playback Rate</h6>
