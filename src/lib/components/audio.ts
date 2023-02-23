8260a2078f191305473a5e748e662c23b9cefc62
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-02-23 13:28:04 +0000

    linting

--- src/lib/components/audio.ts
+++ src/lib/components/audio.ts
@@ -3,7 +3,7 @@ export type PlayerElement = HTMLAudioElement | undefined;
 export function usePlayer<K extends keyof HTMLAudioElement>(
 	el: PlayerElement,
 	key: K,
-	value: HTMLAudioElement[K]
+	value: HTMLAudioElement[K],
 ) {
 	if (!el) return;
 	el[key] = value;
