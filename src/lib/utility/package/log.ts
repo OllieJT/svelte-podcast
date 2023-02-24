fdde14048db7430c5663e4a569de7512d67b721c
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-02-24 19:39:58 +0000

    fixes logger output

--- src/lib/utility/package/log.ts
+++ src/lib/utility/package/log.ts
@@ -13,9 +13,9 @@ export function log(type: Logger, ...content: unknown[]) {
 
 	if (type === 'info' && !dev) return;
 
-	logger('🔊 svelte-podcast: ', content);
+	logger('🔊 svelte-podcast: ', ...content);
 }
 
 export const info = (...content: unknown[]) => log('info', ...content);
-export const warn = (...content: unknown[]) => log('warn', content);
-export const error = (...content: unknown[]) => log('error', content);
+export const warn = (...content: unknown[]) => log('warn', ...content);
+export const error = (...content: unknown[]) => log('error', ...content);
