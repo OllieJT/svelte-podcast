555393c59ffa001560dce258cb3f6ab106e77554
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-03-09 01:04:01 +0000

    Improves navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,63 +1,53 @@
 <script lang="ts">
-	import { MagnifyingGlass } from '@inqling/svelte-icons/heroicon-20-solid';
-	import { Github, Npm } from '@inqling/svelte-icons/simple-icons';
-	// import 'highlight.js/styles/base16/dracula.css';
+	import { page } from '$app/stores';
+	import { Github } from '@inqling/svelte-icons/simple-icons';
+	import clsx from 'clsx';
 	import { AudioLoader } from 'svelte-podcast';
 	import '../app.postcss';
+
+	const page_links = [
+		{ label: 'Docs', href: '/' },
+		{ label: 'Demo', href: '/demo' },
+	] as const;
 </script>
 
-<header class="bg-white shadow-sm lg:static lg:overflow-y-visible" id="navigation">
-	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-		<div class="relative flex justify-between lg:gap-8 xl:grid xl:grid-cols-12">
-			<div class="flex md:absolute md:inset-y-0 md:left-0 lg:static xl:col-span-2">
-				<div class="flex flex-shrink-0 items-center">
-					<a href="/#navigation">
-						<img
-							class="block h-8 w-auto"
-							width={60}
-							height={32}
-							src="/logo.png"
-							alt="Your Company"
-						/>
+<header
+	class="sticky top-0 z-50 border-b border-mono-100 bg-white text-lg leading-none lg:overflow-y-visible"
+	id="navigation"
+>
+	<div class="mx-auto max-w-prose px-4 py-1 sm:py-2 sm:px-6 lg:px-8">
+		<div class="relative grid grid-cols-12 gap-1 text-base md:gap-3">
+			<div class="col-span-2 flex items-center justify-start leading-none">
+				<a href="/#navigation" class="flex-shrink-0 py-1">
+					<img class="block h-8 w-auto" width={60} height={32} src="/logo.png" alt="Your Company" />
+				</a>
+			</div>
+			<div class="col-span-8 flex items-center justify-center leading-none sm:gap-1">
+				{#each page_links as link}
+					{@const is_homepage = link.href === '/' && $page.url.pathname === link.href}
+					{@const is_match = link.href !== '/' && $page.url.pathname.startsWith(link.href)}
+					<a
+						href={link.href}
+						class={clsx(
+							'flex items-center justify-center rounded-md px-3 py-2 text-sm font-medium leading-none ring-inset focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500 focus:ring-offset-2 sm:px-4 sm:text-base',
+							is_homepage || is_match
+								? 'bg-primary-200 text-primary-900'
+								: 'text-mono-500 hover:bg-mono-100 hover:text-mono-900',
+						)}
+					>
+						{link.label}
 					</a>
-				</div>
+				{/each}
 			</div>
-			<div class="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
-				<div
-					class="flex items-center px-3 py-2 sm:py-3 sm:px-6 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0"
-				>
-					<!-- TODO: add search functionality -->
-					<div class="w-full">
-						<label for="search" class="sr-only">Search</label>
-						<div class="relative">
-							<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-								<MagnifyingGlass class="h-5 w-5 text-mono-400" />
-							</div>
-							<input
-								id="search"
-								name="search"
-								class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-mono-900 ring-1 ring-inset ring-mono-300 placeholder:text-mono-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"
-								placeholder="Search"
-								type="search"
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="flex items-center justify-end gap-3 sm:gap-5 xl:col-span-4">
+			<div class="col-span-2 flex items-center justify-end leading-none">
 				<a
 					href="https://github.com/OllieJT/svelte-podcast"
-					class="flex-shrink-0 rounded-full bg-white p-1 text-mono-400 hover:text-mono-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
+					target="_blank"
+					rel="noreferrer"
+					class="flex-shrink-0 rounded-full bg-white p-1 text-mono-500 ring-2 ring-transparent transition-all ease-out hover:text-mono-900 hover:ring-mono-200 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
 				>
-					<span class="sr-only">View notifications</span>
+					<span class="sr-only">View Repository</span>
 					<Github class="h-6 w-6" aria-hidden="true" />
-				</a>
-				<a
-					href="https://www.npmjs.com/package/svelte-podcast"
-					class="flex-shrink-0 rounded-full bg-white p-1 text-mono-400 hover:text-mono-500 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
-				>
-					<span class="sr-only">View notifications</span>
-					<Npm class="h-6 w-6" aria-hidden="true" />
 				</a>
 			</div>
 		</div>
