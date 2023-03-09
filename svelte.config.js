2659a320abb039f2363f69504c82618288fe78cc
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-03-09 11:26:21 +0000

    adds mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
@@ -11,6 +15,7 @@ const config = {
 		preprocess({
 			postcss: true,
 		}),
+		mdsvex(mdsvexConfig),
 	],
 
 	kit: {
