f9d9caf232e472c2029c21d67dbbc1539e021b1e
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-07-10 22:22:11 +0100

    simplify .d.ts

--- src/globals.d.ts
+++
@@ -1,8 +0,0 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-
-// import type { ComponentType, SvelteComponentTyped } from 'svelte';
-
-declare module '*.md' {
-	export default ComponentType<SvelteComponentTyped>;
-}
