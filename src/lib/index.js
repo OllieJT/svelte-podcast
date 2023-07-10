37d2fc90434d9bea48005cfe515c296184e77d92
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-07-10 21:25:57 +0100

    Update js exports

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+export * from './audio';
+export { AudioPlayer as default } from './audio';
+export * from './user';
+export * from './utility';
--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './audio';
-export * from './components';
-export { AudioPlayer as default } from './components';
-export * from './user';
-export * from './utility';
