62dd79b6e22a40ae920794361d62661750e81ca6
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-07-11 22:09:35 +0100

    remove unused code and deps

--- src/routes/+page.server.js
+++
@@ -1,8 +0,0 @@
-import content from '$src/content/docs.md?raw';
-import { use_markdown } from '$src/markdown';
-
-/** @type { import('./$types').PageServerLoad} */
-export const load = async () => {
-	const post = await use_markdown(content);
-	return { html: post.value };
-};
