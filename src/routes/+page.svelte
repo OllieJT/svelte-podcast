972600d420e9ad4b4e2075a9d0a928f773c88c82
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-07-11 22:03:12 +0100

    fixes homepage links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,13 +69,13 @@
 						</p>
 						<div class="mt-10 flex items-center justify-center gap-x-3">
 							<a
-								href="{base}/#docs"
+								href="{base}/setup"
 								class="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
 							>
 								Get started
 							</a>
 							<a
-								href="{base}/#examples"
+								href="{base}/api"
 								class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-mono-600 hover:bg-primary-50 hover:text-primary-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
 							>
 								Examples <span aria-hidden="true">→</span>
